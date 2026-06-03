f7e8c25a75ddf224e632902e26475522829f99d5
nutdaeun <goodnvery@naver.com>
2026-06-03 15:10:59 +0900

    .

--- myproject2/settings.py
+++ myproject2/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from decouple import config
 from pathlib import Path
-import pymysql
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
